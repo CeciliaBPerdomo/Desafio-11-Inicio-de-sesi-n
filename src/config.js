fe073cb3ab00ac5bd6d083c9b746461a93cf62a1
Cecilia <cecilia.perdomo@gmail.com>
2022-06-08 22:01:51 -0300

    commit 10

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     mongoDB: {
-        url: 'mongodb+srv://Cecilia:ceci1984@cluster1.sf6kh.mongodb.net/ecommerce?retryWrites=true&w=majority',
+        url: 'mongodb+srv://Cecilia:ceci1984@cluster1.sf6kh.mongodb.net/?retryWrites=true&w=majority',
         options: {
             useUnifiedTopology: true
         }
